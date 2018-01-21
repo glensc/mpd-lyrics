50c3bbf2c9d15ba1988a927771def714320e5866
Elan Ruusamäe <glen@delfi.ee>
2018-01-21 21:38:27 +0200

    make mpd host configurable

--- lyrics.sh
+++ lyrics.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # https://gist.github.com/febuiles/1549991
-artist=$(mpc -f %artist% | head -n 1)
-title=$(mpc -f %title% | head -n 1)
+
+: ${MPD_HOST=localhost}
+
+artist=$(mpc -h $MPD_HOST -f %artist% | head -n 1)
+title=$(mpc -h $MPD_HOST -f %title% | head -n 1)
 song=$(curl -s --get "https://makeitpersonal.co/lyrics" --data-urlencode "artist=$artist" --data-urlencode "title=$title")
 
 printf "%s - %s\n%s" "$artist" "$title" "$song"
